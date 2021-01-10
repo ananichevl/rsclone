eadceba90ac1f536390c5c84cbb9475f2edb2ca0
Ананичев Леонид Дмитриевич <ananichev.l.dm@sberbank.ru>
2021-01-10 16:31:25 +0300

    init: rsclone trello task

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # trello-clone
+Trello clone project
+## Start
+```npm run start``` - start dev server watch mode localhost:3000 
+
+```npm run build``` - compile project
---
+++ trello-clone/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                        ],
+                    },
+                }, 'eslint-loader']
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    miniCss.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }, {
+                test: /\.(wav|png|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'assets/[hash].[ext]',
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Covid-dashboard',
+            template: 'index.html'
+        }),
+        new miniCss({
+            filename: 'style.css',
+        }),
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        compress: true,
+        port: 3000
+    }
+};
